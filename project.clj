8203dffbe8690ebdaae404669508fbb9eebcd480
Mike Thvedt <mike.thvedt@gmail.com>
2013-04-05 15:14:21 -0500

    Update to work with latest leiningen

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
-(defproject lein-lazytest "1.0.3"
-  :description "Lein plugin for starting LazyTest watch"
+(defproject eightnotrump/lein-lazytest "1.0.5"
+  :description "Lein plugin for Stuart Sierra's lazytest"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[lein-clojars "0.6.0"] [com.stuartsierra/lazytest "1.1.2"]]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [leiningen-core "2.1.0"]
+                 [com.stuartsierra/lazytest "1.1.2"]]
+  :repositories {"stuartsierra-releases" "http://stuartsierra.com/maven2"
+                 "stuartsierra-snapshots" "http://stuartsierra.com/m2snapshots"}
   :eval-in-leiningen true)
--- src/leiningen/lazytest.clj
+++ src/leiningen/lazytest.clj
@@ -1,12 +1,9 @@
 (ns leiningen.lazytest
-  (:use [leiningen.compile :only [eval-in-project]]))
-
+  (:require [leiningen.core.eval :as eval]))
 
 (defn lazytest "Runs lazytest watch on paths supplied in :lazytest-path [\"src\" \"test\"]"  [project]
-  (eval-in-project
+  (eval/eval-in-project
    project      
    `(do        
       (require 'lazytest.watch)       
       (lazytest.watch/start ~(get project :lazytest-path ["src" "test"])))))
-
-
